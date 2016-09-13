42cd8f651b5e10a3fdb5e2a4e6ca4c66d76c2029
Samuel Mullins <Samuel Mullins>
2016-09-12 21:12:17 -0500

    Adding Homework 1

---
+++ assignments/homework-01/myrandom.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/dict/words | myrandom.sh
--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo Please enter arguments.
 read arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9
 let sum=arg1+arg2+arg3+arg4+arg5+arg6+arg7+arg8+arg9
